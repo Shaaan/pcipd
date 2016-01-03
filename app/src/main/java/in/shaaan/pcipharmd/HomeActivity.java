9b7574d6504473b537f4ce3c1a0b2bd783259b3b
Shantanu Londhe <shaaan.xda@gmail.com>
2016-01-04 01:42:02 +0530

    Initial skeleton for application. Add future proofing support

--- app/src/main/java/in/shaaan/pcipharmd/HomeActivity.java
+++ app/src/main/java/in/shaaan/pcipharmd/HomeActivity.java
@@ -1,5 +1,8 @@
 package in.shaaan.pcipharmd;
 
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +11,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -22,11 +27,18 @@ public class HomeActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "View the curriculum of the PharmD course in India ", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
     }
+
+    public void syllabus1(View view) {
+        String url = "https://about.me/shantanulondhe";
+        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        startActivity(i);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
