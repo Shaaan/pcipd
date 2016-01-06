2960165cea942e5617e878e70eef87fc9b3d6b97
Shantanu Londhe <shaaan.xda@gmail.com>
2016-01-06 18:52:12 +0530

    Add fifth year syllabus

--- app/src/main/java/in/shaaan/pcipharmd/FifthYear.java
+++ app/src/main/java/in/shaaan/pcipharmd/FifthYear.java
@@ -1,13 +1,15 @@
 package in.shaaan.pcipharmd;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.customtabs.CustomTabsIntent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class FifthYear extends AppCompatActivity {
+public class FifthYear extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +17,11 @@ public class FifthYear extends AppCompatActivity {
         setContentView(R.layout.activity_fifth_year);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        findViewById(R.id.epi).setOnClickListener(this);
+        findViewById(R.id.clerkship).setOnClickListener(this);
+        findViewById(R.id.tdm).setOnClickListener(this);
+        findViewById(R.id.research).setOnClickListener(this);
+        findViewById(R.id.tp3).setOnClickListener(this);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +33,49 @@ public class FifthYear extends AppCompatActivity {
         });
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+
+        switch (id) {
+            case R.id.epi:
+                String epi = "https://shaaan.github.io/pcipd/syllabus5/epi/";
+                CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+                builder.setStartAnimations(this, R.anim.slide_in_right, R.anim.slide_out_left);
+                builder.setExitAnimations(this, R.anim.slide_in_left, R.anim.slide_out_right);
+                CustomTabsIntent customTabsIntent = builder.build();
+                customTabsIntent.launchUrl(this, Uri.parse(epi));
+                break;
+            case R.id.research:
+                String s = "https://shaaan.github.io/pcipd/syllabus5/research";
+                CustomTabsIntent.Builder builder1 = new CustomTabsIntent.Builder();
+                builder1.setStartAnimations(this, R.anim.slide_in_right, R.anim.slide_out_left);
+                builder1.setExitAnimations(this, R.anim.slide_in_left, R.anim.slide_out_right);
+                CustomTabsIntent customTabsIntent1 = builder1.build();
+                customTabsIntent1.launchUrl(this, Uri.parse(s));
+                break;
+            case R.id.tdm:
+                String s1 = "https://shaaan.github.io/pcipd/syllabus5/tdm";
+                CustomTabsIntent.Builder builder2 = new CustomTabsIntent.Builder();
+                builder2.setStartAnimations(this, R.anim.slide_in_right, R.anim.slide_out_left);
+                builder2.setExitAnimations(this, R.anim.slide_in_left, R.anim.slide_out_right);
+                CustomTabsIntent customTabsIntent2 = builder2.build();
+                customTabsIntent2.launchUrl(this, Uri.parse(s1));
+                break;
+            //// TODO: 6/1/16 Shift this to Fourth Year 
+            
+            /*case R.id.tp3:
+                String s3 = "https://shaaan.github.io/pcipd/syllabus5/epi";
+                CustomTabsIntent.Builder builder3 = new CustomTabsIntent.Builder();
+                builder3.setStartAnimations(this, R.anim.slide_in_right, R.anim.slide_out_left);
+                builder3.setExitAnimations(this, R.anim.slide_in_left, R.anim.slide_out_right);
+                CustomTabsIntent customTabsIntent3 = builder3.build();
+                customTabsIntent3.launchUrl(this, Uri.parse(s3));
+                break;*/
+            default:
         }
     }
 }
