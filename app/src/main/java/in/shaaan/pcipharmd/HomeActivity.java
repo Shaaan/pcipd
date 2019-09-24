739a7a157bc630bc294aeffada5d3f67774e15c3
Shaaan <shaaan.xda@gmail.com>
2019-09-24 17:51:08 +0530

    Optimize imports, fix and clean up code. Also added a chat functionality!
    Signed-off-by: Shantanu Londhe <shaaan.xda@gmail.com>

--- app/src/main/java/in/shaaan/pcipharmd/HomeActivity.java
+++ app/src/main/java/in/shaaan/pcipharmd/HomeActivity.java
@@ -1,16 +1,23 @@
 package in.shaaan.pcipharmd;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.google.android.ads.nativetemplates.NativeTemplateStyle;
 import com.google.android.ads.nativetemplates.TemplateView;
 import com.google.android.gms.ads.AdLoader;
@@ -21,6 +28,22 @@ import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.mikepenz.iconics.IconicsDrawable;
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
+import com.mikepenz.materialdrawer.util.AbstractDrawerImageLoader;
+import com.mikepenz.materialdrawer.util.DrawerImageLoader;
+import com.mikepenz.materialdrawer.util.DrawerUIUtils;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -30,6 +53,11 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     @BindView(R.id.nativeCard_03)
     CardView nativeCard13;
 
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+    String userPhoto;
+    private Drawer result;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +65,8 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         ButterKnife.bind(this);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
         findViewById(R.id.nativeCard_01).setVisibility(View.GONE);
         findViewById(R.id.nativeCard_02).setVisibility(View.GONE);
         nativeCard13.setVisibility(View.GONE);
@@ -46,6 +76,120 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.syllabus4).setOnClickListener(this);
         findViewById(R.id.syllabus5).setOnClickListener(this);
         findViewById(R.id.syllabus6).setOnClickListener(this);
+
+//        Check if the user has signed up for chat
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+
+//        if you want to update the items at a later time it is recommended to keep it in a variable
+        PrimaryDrawerItem item1 = new PrimaryDrawerItem().withIdentifier(1).withName(R.string.drawer_item_syllabus);
+        PrimaryDrawerItem item2 = new PrimaryDrawerItem().withIdentifier(2).withName(R.string.drawer_item_chat);
+
+//      Create the AccountHeader
+        String userName = "Anonymous";
+        String userMail = "jane@doe.com";
+
+        //initialize and create the image loader logic
+        //initialize and create the image loader logic
+        DrawerImageLoader.init(new AbstractDrawerImageLoader() {
+            @Override
+            public void set(ImageView imageView, Uri uri, Drawable placeholder, String tag) {
+                Glide.with(imageView.getContext())
+                        .load(uri)
+                        .apply(new RequestOptions()
+                                .placeholder(placeholder))
+                        .into(imageView);
+            }
+
+            @Override
+            public void cancel(ImageView imageView) {
+                Glide.with(imageView.getContext()).clear(imageView);
+            }
+
+            @Override
+            public Drawable placeholder(Context ctx, String tag) {
+                //define different placeholders for different imageView targets
+                //default tags are accessible via the DrawerImageLoader.Tags
+                //custom ones can be checked via string. see the CustomUrlBasePrimaryDrawerItem LINE 111
+                if (DrawerImageLoader.Tags.PROFILE.name().equals(tag)) {
+                    return DrawerUIUtils.getPlaceHolder(ctx);
+                } else if (DrawerImageLoader.Tags.ACCOUNT_HEADER.name().equals(tag)) {
+                    return new IconicsDrawable(ctx).iconText(" ").backgroundColorRes(com.mikepenz.materialdrawer.R.color.primary).sizeDp(56);
+                } else if ("customUrlItem".equals(tag)) {
+                    return new IconicsDrawable(ctx).iconText(" ").backgroundColorRes(R.color.md_red_500).sizeDp(56);
+                }
+
+                //we use the default one for
+                //DrawerImageLoader.Tags.PROFILE_DRAWER_ITEM.name()
+
+                return super.placeholder(ctx, tag);
+            }
+        });
+
+
+        userPhoto = "https://google.com";
+//      If the user has logged in to chat, set his custom image and name
+        if (mUser != null) {
+            userName = mUser.getDisplayName();
+            userMail = mUser.getEmail();
+            if (mUser.getPhotoUrl() != null) {
+                userPhoto = mUser.getPhotoUrl().toString();
+            }
+
+        }
+
+
+        AccountHeader headerResult = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(R.color.md_white_1000)
+                .addProfiles(
+                        new ProfileDrawerItem().withName(userName).withEmail(userMail).withIcon(userPhoto)
+                )
+                .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
+                    @Override
+                    public boolean onProfileChanged(View view, IProfile profile, boolean currentProfile) {
+                        return false;
+                    }
+                })
+                .withDividerBelowHeader(true)
+                .withSavedInstance(savedInstanceState)
+                .build();
+
+        //create the drawer and remember the `Drawer` result object
+        result = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withAccountHeader(headerResult)
+                .addDrawerItems(
+                        item1,
+                        item2,
+                        new DividerDrawerItem(),
+                        new SecondaryDrawerItem().withName(R.string.action_about)
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        // do something with the clicked item :D
+                        Log.d("Drawer", "Clicked" + position);
+                        switch (position) {
+                            case 1:
+                                Toast toast = Toast.makeText(HomeActivity.this, "Already viewing the syllabus", Toast.LENGTH_SHORT);
+                                toast.show();
+                                break;
+
+                            case 2:
+                                Intent chatIntent = new Intent(HomeActivity.this, ChatActivity.class);
+                                startActivity(chatIntent);
+                                result.closeDrawer();
+                                break;
+                        }
+                        return true;
+                    }
+                })
+                .withFullscreen(true)
+                .withSavedInstance(savedInstanceState)
+                .build();
+
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -210,7 +354,11 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onBackPressed() {
-        super.onBackPressed();
-        finish();
+        if (result != null && result.isDrawerOpen()) {
+            result.closeDrawer();
+        } else {
+            super.onBackPressed();
+//            finish();
+        }
     }
 }
